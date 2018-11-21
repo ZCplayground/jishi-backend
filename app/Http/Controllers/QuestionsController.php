ef39825822a7421b63362334a2fa1533c5738f4f
pandaeathzr <031602215@fzu.edu.cn>
2018-11-21 13:39:52 +0800

    增加一些注释，调整代码结构

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -43,25 +43,28 @@ class QuestionsController extends Controller
         {
             // token验证成功
 
-            $Dimension = 7 ; //定义维度数目为7
-            $Dimension_other =4 ; //定义非主食问题的维度为4
+            $Dimension = 7 ;                //定义维度数目为7
+            $Dimension_other =4 ;           //定义非主食问题的维度为4
 
 
             $front_two = range(1,$Dimension_other);  //利用range()函数产生一个1到$Dimension_other的数组
-            shuffle($front_two);  //利用shuffle()函数将产生的$num数组随机打乱顺序
+            shuffle($front_two);            //利用shuffle()函数将产生的数组随机打乱顺序
 
-            $first_qnum = rand(1,Question::where('attr', $front_two[0])->count()); //获取维度问题数目
-            $first_question = Question::where('attr',$front_two[0])->select('id','content')->offset($first_qnum-1)->limit(1)->first();
+            $first_qnum = rand(1,Question::where('attr', $front_two[0])->count());  //获取维度问题数目后，随机该维度下的某个问题
+            $first_question = Question::where('attr',$front_two[0])->select('id','content')->offset($first_qnum-1)->limit(1)->first(); 
+            //返回问题
 
-            $second_qnum = rand(1,Question::where('attr', $front_two[1])->count()); //获取维度问题数目
+            $second_qnum = rand(1,Question::where('attr', $front_two[1])->count()); //获取维度问题数目后，随机该维度下的某个问题
             $second_question = Question::where('attr',$front_two[1])->select('id','content')->offset($second_qnum-1)->limit(1)->first();
-
+            //返回问题
 
             $third = rand(1,$Dimension-$Dimension_other) + $Dimension_other;
 
-            $third_qnum = rand(1,Question::where('attr', $third)->count()); //获取维度问题数目
+            $third_qnum = rand(1,Question::where('attr', $third)->count()); //获取维度问题数目后，随机该维度下的某个问题
             $third_question = Question::where('attr',$third)->select('id','content')->offset($second_qnum-1)->limit(1)->first();
+            //返回问题
 
+            //根据前两个问题序号调整顺序
             if($first_question->id>$second_question->id)
             {
                 $temp = $second_question->id;
@@ -73,15 +76,16 @@ class QuestionsController extends Controller
                 $first_question->content = $temp;
             }
 
+
             $question_id_list = "$first_question->id,$second_question->id,$third_question->id";
 
-            // 返回question_id_list,三个问题,http 200
+            // 返回question_id_list,三个问题,http 201
             return response()->json([
                 'question_id_list' => $question_id_list,
                 'content1' => $first_question->content,
                 'content2' => $second_question->content,
                 'content3' => $third_question->content
-            ], 200);
+            ], 201);
         }
     }
 }
