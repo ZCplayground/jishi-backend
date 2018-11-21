bd0b5e93143fc66df56650a069708e465eb48d8a
pandaeathzr <031602215@fzu.edu.cn>
2018-11-21 13:33:39 +0800

    (fix)，基本完成请求问题接口，本地测试通过

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -12,7 +12,7 @@ class QuestionsController extends Controller
     public function questionRequest(Request $request) //请求问题
     {
         // 前端发送 json 'user_id'=>用户id,'token'=>token,'number'=>问题数量
-        // 前端返回的应该时id吧？？？？
+    
 
         $data = $request->getContent();
         $data = json_decode($data, true);
@@ -42,12 +42,45 @@ class QuestionsController extends Controller
         else if($ret === 'success')
         {
             // token验证成功
-            $c = Question::where('id', 1)->select('attr','choice')->first();
+
+            $Dimension = 7 ; //定义维度数目为7
+            $Dimension_other =4 ; //定义非主食问题的维度为4
 
 
-            // 成功修改密码。返回http 200
+            $front_two = range(1,$Dimension_other);  //利用range()函数产生一个1到$Dimension_other的数组
+            shuffle($front_two);  //利用shuffle()函数将产生的$num数组随机打乱顺序
+
+            $first_qnum = rand(1,Question::where('attr', $front_two[0])->count()); //获取维度问题数目
+            $first_question = Question::where('attr',$front_two[0])->select('id','content')->offset($first_qnum-1)->limit(1)->first();
+
+            $second_qnum = rand(1,Question::where('attr', $front_two[1])->count()); //获取维度问题数目
+            $second_question = Question::where('attr',$front_two[1])->select('id','content')->offset($second_qnum-1)->limit(1)->first();
+
+
+            $third = rand(1,$Dimension-$Dimension_other) + $Dimension_other;
+
+            $third_qnum = rand(1,Question::where('attr', $third)->count()); //获取维度问题数目
+            $third_question = Question::where('attr',$third)->select('id','content')->offset($second_qnum-1)->limit(1)->first();
+
+            if($first_question->id>$second_question->id)
+            {
+                $temp = $second_question->id;
+                $second_question->id = $first_question->id;
+                $first_question->id = $temp ;
+
+                $temp = $second_question->content;
+                $second_question->content = $first_question->content;
+                $first_question->content = $temp;
+            }
+
+            $question_id_list = "$first_question->id,$second_question->id,$third_question->id";
+
+            // 返回question_id_list,三个问题,http 200
             return response()->json([
-                'info' => $c
+                'question_id_list' => $question_id_list,
+                'content1' => $first_question->content,
+                'content2' => $second_question->content,
+                'content3' => $third_question->content
             ], 200);
         }
     }
