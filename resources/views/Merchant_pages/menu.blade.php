10bfedf3e98dc637a2efef003cf062caba5e7d7a
Syxxx <303241829@qq.com>
2018-12-20 22:12:47 +0800

    fix:修复了左上角logo的bug
    logo点击后跳转到主页

--- resources/views/Merchant_pages/menu.blade.php
+++ resources/views/Merchant_pages/menu.blade.php
@@ -18,7 +18,7 @@
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="main.html">即食商家端 </a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="{{ URL::route('Merchant_main')}}">即食商家端 </a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
                         <ul class="nav nav-icons">
                             <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
